58be386ca0b2c38613f6a1da5dfce14632465fb0
Paul Szerlip <paul.szerlip@knights.ucf.edu>
2014-04-11 13:07:22 -0400

    Modified express serve for using mongo api

--- html/expressServe.js
+++ html/expressServe.js
@@ -3,11 +3,15 @@ var express = require('express');
 var path = require('path');
 module.exports = pbServer;
 
-function pbServer()
+
+var winsave = require('win-save');
+
+function pbServer(finished)
 {
 	var app = express();
 
 	console.log("Build located: ", path.resolve(__dirname, "../build"));
+
 
 	app.use("/build", express.static(path.resolve(__dirname, "../build")));
 	app.use("/js", express.static(path.resolve(__dirname, "../libs")));
@@ -18,10 +22,26 @@ function pbServer()
 		res.sendfile(__dirname + "/pbHome.html");
 	});
 
-	//otherwise, just serve what's in this folder as is
-	app.use(express.static(__dirname));
+	///
+  	winsave.createWinApp('pic', 
+  		{artifactType: "picArtifact", 
+  		directory: path.resolve(__dirname, "../server"), seedDirectory: './seeds', schemaDirectory: './schemas'},
+  		 function(err, apiApp, mongooseConnection)
+	    {
+	        if(err){
+	            finished(err);
+	            throw new Error(err);
+	        }
 
-	return app;
+	        //use this api thing
+	        app.use(apiApp);
+
+
+			//otherwise, just serve what's in this folder as is
+			app.use(express.static(__dirname));
+
+	        finished(undefined, app);
+	    });
 }
 
 
